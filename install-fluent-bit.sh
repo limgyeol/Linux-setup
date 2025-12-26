66c50da1ded5a58840a6bb1055a4bcf8f2964b9d
limgyeol <limg8299@daum.net>
2025-12-26 19:36:38 +0900

    Update fluent-bit.sh

--- install-fluent-bit.sh
+++ install-fluent-bit.sh
@@ -1,6 +1,4 @@
 curl https://raw.githubusercontent.com/fluent/fluent-bit/master/install.sh | sh
 systemctl enable --now fluent-bit
 ln -s /opt/fluent-bit/bin/fluent-bit /usr/local/bin/fluent-bit
-cat /etc/fluent-bit/fluent-bit.conf | egrep -v '^$|#'
 fluent-bit -c /etc/fluent-bit/fluent-bit.conf --dry-run
-
