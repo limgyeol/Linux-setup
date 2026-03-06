730b7348cd95920f762a7ea335b2baeae009d321
limgyeol <limg8299@daum.net>
2026-03-06 18:20:09 +0900

    feat: Add files

--- jenkins.sh
+++ jenkins.sh
@@ -1,3 +1,6 @@
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 dnf install jenkins -y
 systemctl enable jenkins
 systemctl start jenkins
