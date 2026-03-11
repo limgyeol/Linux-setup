eef6e9cad4af56fcdfdf7d255a7a368ff0c38563
limgyeol <limg8299@daum.net>
2026-03-11 14:36:11 +0900

    feat: Add Files

--- codedeploy-agent.sh
+++ codedeploy-agent.sh
@@ -4,13 +4,3 @@ chmod +x ./install
 ./install auto
 systemctl start codedeploy-agent
 systemctl enable codedeploy-agent
-
-dnf update -y
-dnf install ruby wget -y
-cd /home/ec2-user
-# 서울 리전 기준 설치 파일 다운로드
-wget https://aws-codedeploy-ap-northeast-2.s3.ap-northeast-2.amazonaws.com/latest/install
-chmod +x ./install
-./install auto
-# 상태 확인 (active/running이 떠야 함)
-service codedeploy-agent status
