9f0d948ebee4fc76f5157f544dbcd9ec9362de1d
limgyeol <limg8299@daum.net>
2026-04-14 18:52:50 +0900

    feat:Add files

--- eksctl.sh
+++ eksctl.sh
@@ -1,4 +1,4 @@
-ARCH=x86
+ARCH=amd64
 PLATFORM=$(uname -s)_$ARCH
 curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
 curl -sL "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_checksums.txt"| grep $PLATFORM | sha256sum --check
